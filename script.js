8082f1b909ad487cc614570007d262808901da12
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2021-11-23 09:12:37 +0100

    Update main page

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let info = {};
-
-fetch('https://api.github.com/users/Valate75', { 
+fetch('https://api.github.com/users/Valate75', {
     headers: {'Accept' : 'application/vnd.github.v3+json'}})                                        // Fetch Data from GitHub
    .then( response => response.json())                                                              // Converting the response to a JSON object
-   .then( data => info = data)                                                                      // Convert data into a global var info (for multiple uses)
-   .then( data => document.getElementById('bio').textContent = info.bio)                            // Send bio to the HTML
-   .then( data => document.getElementById('name').textContent = `${info.login} (${info.name})`)     // Send login and name to the HTML
+   .then( data => { //document.getElementById('bio').textContent = data.bio
+                    document.getElementById('name').textContent = `${data.login} (${data.name})`
+                  })                                                                                // send data to the HTML
    .catch( error => console.error(error));                                                          // Catch Errors
+
+fetch('https://raw.githubusercontent.com/Valate75/Valate75/main/README.md')                         // Fetch Data from GitHub
+    .then( response => response.text())                                                             // Converting the response to text
+    .then( data => {                                                                                // Send text to the HTML
+            let bc = false; let text = "";
+            for(let i = 0; i < data.length; i++) // Convery the md # to HTML bold
+            {
+                char = data.charAt(i)
+                if(char == '#') { char = "<b>"; i++; bc = !bc; }
+                else if(char == '\n' && bc) { char = "</b>"; i--; bc = !bc; }
+                text += char;
+            }
+            document.getElementById('info').innerHTML = text
+        })
+    .catch( error => console.error(error));                                                         // Catch Errors
