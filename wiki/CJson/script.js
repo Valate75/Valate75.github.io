dfef4d9688e3412a618cfb8c69bfb4d75f041dc8
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2021-11-25 12:27:26 +0100

    Added the code to CJson
    Again it still looks bad (I will one day fix it up)

--- wiki/CJson/script.js
+++ wiki/CJson/script.js
@@ -7,7 +7,7 @@ fetch('https://raw.githubusercontent.com/Valate75/Cjson/main/cheatsheet.md')    
                 char = data.charAt(i)
                 if(char == '#') { char = "<b>"; i++; bc = !bc; } // Make titles bold
                 else if(char == '\n' && bc) { char = "</b>"; i--; bc = !bc; }
-                else if(char == '/' && data.charAt(i + 1) == '/') { char = "<span>//"; i+=2; sc = !sc; } // Make text another colour
+                else if(char == '/' && data.charAt(i + 1) == '/') { char = "<span>// "; i+=2; sc = !sc; } // Make text another colour
                 else if(char == '\n' && sc) { char = "</span>"; i--; sc = !sc; }
                 text += char;
             }
@@ -29,3 +29,35 @@ fetch('https://raw.githubusercontent.com/Valate75/Cjson/main/README.md')        
             document.getElementById('info').innerHTML = text                                        // Send text to the HTML
         })
     .catch( error => console.error(error));                                                         // Catch Errors
+
+fetch('https://raw.githubusercontent.com/Valate75/Cjson/main/json.h')                               // Fetch Data from GitHub
+    .then( response => response.text())                                                             // Converting the response to text
+    .then( data => {                                                                                // Edit text
+            let bc = false; let sc = false; let text = "";
+            for(let i = 0; i < data.length; i++) // Format the text better
+            {
+                char = data.charAt(i)
+                if(char == '/' && data.charAt(i + 1) == '/') { char = "<span>// "; i+=2; sc = !sc; } // Make text another colour
+                else if(char == '\n' && sc) { char = "</span>"; i--; sc = !sc; }
+                text += char;
+            }
+            document.getElementById('code').innerHTML = text                                        // Send text to the HTML
+        })
+    .catch( error => console.error(error));                                                         // Catch Errors
+
+
+let extended = false;
+function openCode()
+{
+    let getElement = document.getElementById('code');
+    if (extended)
+    {
+        getElement.style.visibility = 'hidden';
+        extended = !extended;
+    }
+    else 
+    {
+        getElement.style.visibility = 'visible';
+        extended = !extended;
+    }
+}
