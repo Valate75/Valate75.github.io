288d2df1be98311cca0d159b998548366b9a169d
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2021-11-29 22:28:43 +0100

    Removed the wiki subpage

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+// Name And Username
 fetch('https://api.github.com/users/Valate75', {
     headers: {'Accept' : 'application/vnd.github.v3+json'}})                                        // Fetch Data from GitHub
    .then( response => response.json())                                                              // Converting the response to a JSON object
-   .then( data => { //document.getElementById('bio').textContent = data.bio
-                    document.getElementById('name').textContent = `${data.login} (${data.name})`
-                  })                                                                                // send data to the HTML
+   .then( data => document.getElementById('name').textContent = `${data.login} (${data.name})`)     // send data to the HTML
    .catch( error => console.error(error));                                                          // Catch Errors
 
+// Readme.md
 fetch('https://raw.githubusercontent.com/Valate75/Valate75/main/README.md')                         // Fetch Data from GitHub
     .then( response => response.text())                                                             // Converting the response to text
     .then( data => {                                                                                // Edit text
